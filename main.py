c4e6e7df96e7758fa1636b62b214c0e28523c1b9
Robin-Britz <36510262+Robin-Britz@users.noreply.github.com>
2025-02-19 08:08:39 +0200

    Layout complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from lib2to3.pgen2.pgen import PgenGrammar
 from tkinter import PhotoImage
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -6,30 +7,23 @@ BACKGROUND_COLOR = "#B1DDC6"
 # MAIN WINDOW
 root = tkinter.Tk()
 root.title("Flash Card App")
-root.config(bg=BACKGROUND_COLOR)
+root.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# FRAME INSIDE WINDOW
-frame = tkinter.Frame(root, padx=50, pady=50)
-frame.grid()
-
-# WIDGETS IN FRAME
-title_label = tkinter.Label(frame)
-title_label.config(text="title")
-title_label.grid(row=0, column=1)
-
-answer_label = tkinter.Label(frame)
-answer_label.config(text="WORD")
-answer_label.grid(row=1, column=1)
-
-# BUTTON IMAGES
-incorrect_btn_image = PhotoImage(file="./images/wrong.png")
-correct_btn_image = PhotoImage(file="./images/right.png")
+# WIDGETS INSIDE CANVAS
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_front = tkinter.Canvas(root, width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
+card_front.create_image(400, 263, image=card_front_img)
+card_front.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+card_front.create_text(400, 263, text="Word", font=("Arial", 40, "bold"))
+card_front.grid(row=0, column=0, columnspan=2)
 
 # BUTTONS
-incorrect_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=incorrect_btn_image, bg="#000000")
-incorrect_btn.grid(row=2, column=0)
+incorrect_btn_img = PhotoImage(file="./images/wrong.png")
+correct_btn_img = PhotoImage(file="./images/right.png")
+incorrect_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=incorrect_btn_img)
+incorrect_btn.grid(row=1, column=0)
 
-correct_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=correct_btn_image, bg="#000000")
-correct_btn.grid(row=2, column=1)
+correct_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=correct_btn_img)
+correct_btn.grid(row=1, column=1)
 
 root.mainloop()
