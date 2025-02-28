4b3f01be84e43c33448a6c8c4d7972a43f9bb3f6
Robin-Britz <36510262+Robin-Britz@users.noreply.github.com>
2025-02-28 15:01:07 +0200

    Correct and Incorrect functionality updated
    Must rethink

--- main.py
+++ main.py
@@ -25,14 +25,20 @@ title = "French"
 word = "English"
 
 
-def random_word():
-    title = random.choice(word_list)
+def incorrect():
+    title = card_front.itemcget("title", "text")
     word = dict_list[title]
     card_front.delete("title")
     card_front.delete("word")
     card_front.create_text(400, 150, text=f"{title}", font=("Arial", 40, "italic"), tags="title")
     card_front.create_text(400, 263, text=f"{word}", font=("Arial", 40, "bold"), tags="word")
-    card_front.grid(row=0, column=0, columnspan=2)
+
+
+def correct():
+    title = random.choice(word_list)
+    card_front.delete("title")
+    card_front.delete("word")
+    card_front.create_text(400, 150, text=f"{title}", font=("Arial", 40, "italic"), tags="title")
 
 
 # WIDGETS INSIDE CANVAS
@@ -45,11 +51,11 @@ card_front.grid(row=0, column=0, columnspan=2)
 
 # BUTTONS
 incorrect_btn_img = PhotoImage(file="./images/wrong.png")
-incorrect_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=incorrect_btn_img, command=random_word)
+incorrect_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=incorrect_btn_img, command=incorrect)
 incorrect_btn.grid(row=1, column=0)
 
 correct_btn_img = PhotoImage(file="./images/right.png")
-correct_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=correct_btn_img, command=random_word)
+correct_btn = tkinter.Button(root, borderwidth=0, highlightthickness=0, image=correct_btn_img, command=correct)
 correct_btn.grid(row=1, column=1)
 
 root.mainloop()
